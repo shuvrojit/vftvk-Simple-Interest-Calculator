6d64cd919e7bb08ec7fe78b979e269d17aca687c
Shuvrojit <jsuvro17@gmail.com>
2022-05-17 06:31:31 +0600

    Finished Product

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+var principle = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = (principle * rate * years ) / 100;
+var year = new Date().getFullYear() + parseInt(years);
+var html_update = "<p>If you deposit <mark>"+principle+"</mark><br>at an interest rate of <mark>"+rate+"</mark><br>You will receive an amount of <mark>"+interest+"</mark><br>in the year "+years+"<br></p>";
+
+function compute() {
+    document.getElementById("result").innerHTML = html_update;
 }
-        
+
+function updateRate() {
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rangeval").innerText=rateVal;
+}
