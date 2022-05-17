6b922a95755485939aaf8378753a381105000620
Shuvrojit <jsuvro17@gmail.com>
2022-05-17 06:56:43 +0600

    finished now

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = (principle * rate * years ) / 100;
 var year = new Date().getFullYear() + parseInt(years);
-var html_update = "<p>If you deposit <mark>"+principle+"</mark><br>at an interest rate of <mark>"+rate+"</mark><br>You will receive an amount of <mark>"+interest+"</mark><br>in the year "+years+"<br></p>";
+var html_update = "<p>If you deposit <mark>"+principle+"</mark><br>at an interest rate of <mark>"+rate+"</mark><br>You will receive an amount of <mark>"+interest+"</mark><br>in the year <mark>"+years+"</mark><br></p>";
 
 function compute() {
     document.getElementById("result").innerHTML = html_update;
